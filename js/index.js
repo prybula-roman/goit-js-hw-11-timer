d4c3021499fb7f00320e5110c0fae1b5de8584ea
prybula_roman <roman.bismail@gmail.com>
2021-11-04 16:55:15 +0200

    4.11.2021

--- js/index.js
+++ js/index.js
@@ -1,25 +1,45 @@
-const timer = {
+import template from "../template/template.hbs";
+
+const listTime = document.querySelector(".timer");
+class Timer {
+  endTime;
+  currentTime;
+  constructor(stopTime) {
+    this.endTime = new Date(stopTime);
+    this.currentTime = Date.now();
+    if (this.endTime.getTime() > this.currentTime) {
+      this.start();
+    }
+  }
   start() {
-    const startTime = Date.now();
     setInterval(() => {
-      const currentTime = Date.now();
-      const deltaTime = currentTime - startTime;
-      const timeComponents = getTimeComponents(deltaTime);
-      console.log(timeComponents);
+      this.currentTime = Date.now();
+      this.deltaTime = this.endTime.getTime() - this.currentTime;
+      this.timeComponents = this.getTimeComponents(this.deltaTime);
+      // console.log(`*********`, this.timeComponents);
     }, 1000);
-  },
-};
+  }
 
-function getTimeComponents(time) {
-  const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-  const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-  return { days, hours, mins, secs };
+  getTimeComponents(time) {
+    const days = Math.floor(time / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((time % (1000 * 60)) / 1000);
+    return { days, hours, mins, secs };
+  }
 }
 
-timer.start();
+const timer = new Timer("2021-11-05 15:25:00");
+
+const timeComponents = () => {
+  setInterval(() => {
+    console.log(`TimeComponents::   ${timer.timeComponents.days}`);
+    console.log(`                   ${timer.timeComponents.hours}`);
+    console.log(`                   ${timer.timeComponents.mins}`);
+    console.log(`                   ${timer.timeComponents.secs}`);
+  }, 1000);
+};
+
+//timeComponents();
+//timer.start();
+//timer.getDeltaTime();
